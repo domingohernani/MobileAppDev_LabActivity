5611d3e2fd83da0bc3ea176f26c6ad1382a6247b
domingohernani <hernani.domingo1@gmail.com>
2023-09-19 20:11:28 +0800

    Details was added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp());
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Flutter Basics!"),
+        ),
+        body: const Center(
+          child: Column(
+            mainAxisAlignment:
+                MainAxisAlignment.center, // Vertically centers the Column
+            crossAxisAlignment:
+                CrossAxisAlignment.center, // Horizontally centers the children
+            children: [
+              Text("Hello! I am Hernani Calpito Domingo"),
+              Text("I'm from Urdaneta City Pangasinan"),
+              Text("Dreaming to be a full-stack developer someday"),
+            ],
+          ),
+        ),
+      ),
+    ),
+  );
 }
